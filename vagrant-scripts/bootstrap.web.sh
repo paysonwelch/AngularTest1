0f52f6777153ebe23ca961027735ced469c6de3b
pwelchnbt <payson.welch@nbtsolutions.com>
2014-08-22 11:06:21 -0400

    Added vagrant file for installing httpd and nodejs

---
+++ vagrant-scripts/bootstrap.web.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y httpd
+sudo yum install -y php php-myqsl
+sudo service httpd start
+sudo chkconfig httpd on
+yum install -y gcc-c++ openssl-devel
+
+cd /usr/local/src
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+sudo install nodejs npm
+
+
+
+#cd /usr/local/src
+#sudo wget http://nodejs.org/dist/node-latest.tar.gz
+#sudo tar zxvf node-latest.tar.gz
+#cd node-v*
+#sudo ./configure
+#sudo make
+#sudo make install
+
