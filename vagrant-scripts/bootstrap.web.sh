22a1d564b084c51d13a1b47fd8bcc10cf7aaf138
paysonwelch <payson.welch@nbtsolutions.com>
2014-08-22 11:31:07 -0400

    fixed the bootstrap file for nodejs and npm

--- vagrant-scripts/bootstrap.web.sh
+++ vagrant-scripts/bootstrap.web.sh
@@ -10,7 +10,7 @@ cd /usr/local/src
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
-sudo install -y nodejs npm
+sudo yum install -y nodejs npm
 
 
 
